00ee647b3de8e3f0fb210818e1ef46fbc0f4cf45
sumon <mdsumonshik1@gmail.com>
2024-08-30 22:31:12 +0600

    Select Multipol Image
    And Insert DataBase
    
    show Image

--- Multipol Image Upload/image.php
+++ Multipol Image Upload/image.php
@@ -1,6 +1,34 @@
-<form action="store.php" method="POST" enctype="multipart/form-data">
-    <label for="">Image</label>
-    <input type="file" name="iamge[]" multiple="">
+<?php 
+    $conn = mysqli_connect("localhost","root","","multi_image");
+    $view = "SELECT * FROM image";
+    $result = mysqli_query($conn,$view);
+?>
 
-    <button type="submit" name="save">Upload</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="store.php" method="POST" enctype="multipart/form-data">
+        <label for="">Image</label>
+        <input type="file" name="iamge[]" multiple="">
+
+        <button type="submit" name="save">Upload</button>
+    </form>
+
+    <?php foreach($result as $results):
+        $explode = explode(',',$results['image']);
+        foreach($explode as $image): 
+            if(!$image ==','){
+            
+            }else{
+    ?>
+    <img src="image/<?php echo $image;?>" alt="" style="width: 100px; height: 100px; margin-top:50px;">
+    <?php } endforeach; endforeach;?>
+
+    
+</body>
+</html>
--- Multipol Image Upload/store.php
+++ Multipol Image Upload/store.php
@@ -7,11 +7,27 @@
         $location = "./image/";
 
         if($image){
-
+            $num = 1;
+            $com_name = "";
             foreach($image_name as $key => $value){
-                $target_path = $location . $value;
+
+                $explode = explode('.',$value);
+                $extrention = end($explode);
+                $coustome_name = date("d-M-Y").time()."-".$num ++.".".$extrention;
+                $target_path = $location . $coustome_name;
+
                 move_uploaded_file($tmp_name[$key],$target_path);
-            }       
-        }    
+
+                $com_name = $com_name ."," .$coustome_name;
+            }
+            
+            $conn = mysqli_connect("localhost","root","","multi_image");
+
+            $insert = "INSERT INTO image(image)VALUE('$com_name')";
+            mysqli_query($conn,$insert);
+            header("location: http://localhost/Chat%20GPT/Mujltipol%20Image%20Upload/image.php");
+        }
+        
     }
+
 ?>
